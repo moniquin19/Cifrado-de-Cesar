6cb851e53562384e3d2156427f357296f9484df1
Mónica Ojeda <monica2519@gmail.com>
2017-10-04 09:04:55 -0300

    Desarrollo del Código

---
+++ main.js
@@ -0,0 +1,18 @@
+function first(opciones){
+	do{
+		/* Aqui el usuario debe ingesar una frase*/
+		var input = prompt("Por favor ingrese una frase");
+		/* Si el usuario no ingresa nada*/
+		if (input != ""){
+			return prompt("Por favor ingrese una frase")
+			}else if(input = ""){
+				return prompt("Deseas que tu frase sea Cifrada marca 1 Descrifrada marca 2");	
+			} while (input == "" || (input != "1" && input != "2"));
+		}
+    }
+	
+		
+
+function dev(){
+
+}
